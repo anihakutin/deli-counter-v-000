aa4e2c0bd45007bb1ad66798f766eeb0919c9d9f
Heshie Brody <4hbrody@gmail.com>
2021-04-21 01:43:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 def now_serving(katz_deli)
   if katz_deli.empty? == false
     puts "Currently serving #{katz_deli.first}."
-    katz_deli.first.unshift
+    katz_deli.first.shift
   else
     puts "There is nobody waiting to be served!"
   end
